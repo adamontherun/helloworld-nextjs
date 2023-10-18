afc875ec4f1b6b6d7fcf4caff37eb247f62b36db
Adam Smith <adaminhonolulu@gmail.com>
2023-10-17 19:36:21 -1000

    adds ability to navigate to the name page

--- src/pages/page2/index.tsx
+++ src/pages/page2/index.tsx
@@ -4,14 +4,14 @@ export default function Home() {
     const router = useRouter();
 
     const handleClick = () => {
-        router.push('/');
+        router.push('/page2/adam');
     };
     return (
         <main className="flex flex-col items-center">
             <div>Page 2</div>
             <div>
                 <button className="text-cyan-500" onClick={handleClick}>
-                    Go back to page 1
+                    Go back to Adam page
                 </button>
             </div>
         </main>
